e7c08694fcc1f8c5eaf131f50da68c6ae6329066
Vida Nsiah <vidansiah@vidas-air.home>
2023-01-27 19:56:03 +0000

    creating variables

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+//I will be creating  a weather dashboard with form inputs
+//when a city is search, this should show the current and future condition for that city
+  
+
+//setting global variables for API keys
+
+let APIkey = "575a1c7b796e55e547288cbfb76ff8e8";
+let currentCity = "";
+let lastCity = "";
+let searchButton = $("#search-button");
+let searchCity = $("#search-city")
+
+
+
+
+//creating current condition
+// let getCurrentConditions = (event) => 
+//     let city = $('#search-city').val();
+//     currentCity = $('#search-city').val();
+
+
+// }
