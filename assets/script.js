3d5f0dc8698850757312ccfc2809c853e6ce61e1
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-01-28 08:29:53 +0000

    chosing first city form cities arrays

--- assets/script.js
+++ assets/script.js
@@ -10,13 +10,28 @@ let lastCity = "";
 let searchButton = $("#search-button");
 let searchCity = $("#search-city")
 
+
+// using fetch to find a city
 fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${APIkey}`)
-    //  .then(response => response.json())
-    //  .then(data => {
+     .then(response => response.json())
+     .then(cities => {
+//chosing a city from the cities found in the arrays
+        let firstcity = cities [0]
+        console.log(firstcity.lat);
+        console.log(firstcity.lon);
 
-    //     console.log(data);
 
- 
+
+     })
+
+ fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=44.348lon=10.998appid=${APIkey}`)  
+ .then(response => response.json())
+ .then(data => {
+
+    console.log(data);     
+
+
+     })
 
 
 //creating current condition
