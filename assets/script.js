4b6cc7ea9e4f3ec4f3def9f4ba5d9b3b62af14c9
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-03 19:15:54 +0000

    getting temp values

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //I will be creating  a weather dashboard with form inputs
 //when a city is search, this should show the current and future condition for that city
-  
+
 
 //setting global variables
 
@@ -8,41 +8,45 @@ let searchCity = $("#search-city");
 let APIkey = "575a1c7b796e55e547288cbfb76ff8e8";
 let btn = document.querySelector('.btn');
 let localStoreList = [];
+let todayEl = document.querySelector("#today")
+let tempEl = document.querySelector("#temperature")
+
 
 // using fetch to find a city (create an eventlisterner for the fetch)
 
-btn.addEventListener('click', function(event){
+btn.addEventListener('click', function (event) {
      event.preventDefault();
      // const inputVal = input.value
      console.log("CLICK");
-          var srchInput = document.querySelector("#search-input").value;
-          console.log("INPUT: ", srchInput);
-   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${srchInput}&limit=5&appid=${APIkey}`)
-   .then(response => response.json())
-   .then(cities => {
-//chosing a city from the cities found in the arrays
-        let firstcity = cities [0]
-        console.log(firstcity.lat);
-        console.log(firstcity.lon);
+     var srchInput = document.querySelector("#search-input").value;
+     console.log("INPUT: ", srchInput);
+     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${srchInput}&limit=5&appid=${APIkey}`)
+          .then(response => response.json())
+          .then(cities => {
+               //chosing a city from the cities found in the arrays
+               let firstcity = cities[0]
+               console.log(firstcity.lat);
+               console.log(firstcity.lon);
 
-        return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstcity.lat}&lon=${firstcity.lon}&appid=${APIkey}`)  
-      
-
-     })
-
- 
- .then(response => response.json())
- .then(data => {
-
-    console.log(data);     
+               return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstcity.lat}&lon=${firstcity.lon}&appid=${APIkey}&units=metric`)
 
 
-     })
+          })
+
+
+          .then(response => response.json())
+          .then(data => {
+               console.log(data)
+               //     console.log(data.list[8].weather[0]); 
+                  tempEl.textContent =  ` ${data.list[0].main.temp}`;
+
+
+          })
      localStoreList.push(srchInput)
      for (let i = 0; i < localStoreList.length; i++) {
           localStorage.setItem(i, JSON.stringify(localStoreList[i]));
      }
-     
+
 })
 
 
@@ -53,4 +57,6 @@ var listBtn = document.createElement("button");
 srchedCityList.appendChild(listBtn);
 
 
- 
+
+
+
